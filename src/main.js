e23e64ecb8e291e1e455765616ce488ef14f500e
Ever Leiva <everleiva21@gmail.com>
2020-11-14 11:27:57 -0300

    Started app w/ vue2

--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
+import { LMap, LTileLayer, LMarker } from 'vue2-leaflet';
+import 'leaflet/dist/leaflet.css';
 
-createApp(App).mount('#app')
+Vue.component('l-map', LMap);
+Vue.component('l-tile-layer', LTileLayer);
+Vue.component('l-marker', LMarker);
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
+
+
