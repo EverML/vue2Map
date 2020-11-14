438ff6ee74b31aee34efeccead1f9e7bca034413
Ever Leiva <everleiva21@gmail.com>
2020-11-14 15:29:00 -0300

    changes in layout

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,55 +1,71 @@
 <template>
   <div class="container">
-      <div class="search-card">
-        <input class="search" type="text" placeholder="Search User....">
-      </div>
+    <div class="search-card">
+      <input
+        v-model="text"
+        @input="doSomething"
+        class="search"
+        type="text"
+        placeholder="Search User...."
+      />
+    </div>
 
     <l-map
       :zoom="zoom"
       :center="center"
-      :bounds="bounds"
-      :max-bounds="maxBounds"
+      :options="mapOptions"
+      @update:center="centerUpdate"
+      @update:zoom="zoomUpdate"
     >
-      <l-tile-layer :noWrap="noWrap" :url="url" :attribution="attribution" />
-      <l-marker :lat-lng="marker" />
+      <l-tile-layer :noWrap="noWrap" :url="url" />
     </l-map>
     <div class="info">
-      Marker is placed at {{ marker.lat }}, {{ marker.lng }}, bounds are
-      {{ bounds }}
-      <br>
+      Center is {{ center.lat }}, {{ center.lng }}, bounds are {{ bounds }},
+      zoom is {{ zoom }}, text is: {{ text }}
     </div>
   </div>
 </template>
 
 <script>
 import { latLngBounds, latLng } from "leaflet";
-import { LMap, LTileLayer, LMarker } from "vue2-leaflet";
+import { LMap, LTileLayer } from /*, LMarker, LPopup, LTooltip*/ "vue2-leaflet";
 
 export default {
-  name: "SetBounds",
+  name: "Map",
   components: {
     LMap,
     LTileLayer,
-    LMarker,
   },
   data() {
     return {
-      center: [47.41322, -1.219482],
+      text: "",
+      zoom: 3.5,
+      center: latLng(-5.317854245891062, -44.219482),
+      url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+      showParagraph: false,
+      noWrap: true,
+      mapOptions: {
+        zoomSnap: 0.5,
+      },
+      showMap: true,
       bounds: latLngBounds([
         [-90, -180],
         [90, 180],
       ]),
-      maxBounds: latLngBounds([
-        [-90, -180],
-        [90, 180],
-      ]),
-      url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-      attribution:
-        '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-      marker: latLng(47.41322, -1.219482),
-      noWrap: true,
-      zoom: 5,
     };
+  },
+  methods: {
+    zoomUpdate(zoom) {
+      console.log(">>>zooom" + zoom);
+      this.zoom = zoom;
+    },
+    centerUpdate(center) {
+      console.log(">>>center" + center);
+      this.center = center;
+    },
+    doSomething() {
+      console.log(this.text);
+    },
   },
 };
 </script>
@@ -60,39 +76,37 @@ export default {
   height: 100vh;
 }
 
-.info{
-    z-index:1000;
-    position: fixed;
-    bottom: 20px;
-    margin-left:40px;
-    margin-right:20px;
-    border-radius:5px;
-    padding: 10px 0px 10px 10px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    background-color: #fff;
-    width: 90%;
+.info {
+  z-index: 1000;
+  position: fixed;
+  bottom: 20px;
+  margin-left: 40px;
+  margin-right: 20px;
+  border-radius: 5px;
+  padding: 10px 0px 10px 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #fff;
+  width: 90%;
 }
 
+.search-card {
+  z-index: 1000;
+  position: fixed;
+  top: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
-.search-card{
-  
-    z-index:1000;
-    position: fixed;
-    top: 20px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    
-    width: 100%;
+  width: 100%;
 }
 
-.search{
-  display:flex;
-  border:2px solid #AAA;
-  padding:10px;
-  border-radius:5px;
+.search {
+  display: flex;
+  border: 2px solid #aaa;
+  padding: 10px;
+  border-radius: 5px;
   padding: 10px 0px 10px 10px;
 }
 </style>
