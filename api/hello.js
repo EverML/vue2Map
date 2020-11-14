df60c5b82ae98f3a9ea8d0049ae71cd71ba65891
Ever Leiva <everleiva21@gmail.com>
2020-11-14 17:06:19 -0300

    refactor to try catch

--- api/hello.js
+++ api/hello.js
@@ -2,8 +2,12 @@ module.exports = (req, res) => {
     const API_URL = "https://bio.torre.co/api/bios";
     const { username } = req.query;
 
-    const response = await fetch(`${API_URL}/${username}`);
-    const data = await response.json();
+    try {
+        const response = await fetch(`${API_URL}/${username}`);
+        const data = await response.json();
+        return res.send(data);
+    } catch (error) {
+        res.send(error);
+    }
     
-    return res.send(data);
 }
