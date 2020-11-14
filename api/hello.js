efa9fc1438c528447bbe67499617d5abec08b2a2
Ever Leiva <everleiva21@gmail.com>
2020-11-14 17:01:42 -0300

    Testing serverless fetch

--- api/hello.js
+++ api/hello.js
@@ -1,4 +1,9 @@
 module.exports = (req, res) => {
-    const { name = 'World' } = req.query
-    res.status(200).send(`Hello ${name}!`)
+    const API_URL = "https://bio.torre.co/api/bios";
+    const { username } = req.query;
+
+    const response = await fetch(`${API_URL}/${username}`);
+    const data = await response.json();
+    
+    return res.send(data);
 }
