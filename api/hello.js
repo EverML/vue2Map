397da93081802bc8560453eabf3084e97004bd5c
Ever Leiva <everleiva21@gmail.com>
2020-11-14 23:08:23 -0300

    Small changes on api

--- api/hello.js
+++ api/hello.js
@@ -1,13 +1,26 @@
+
+const  axios  = require("axios");
+
 module.exports = async (req, res) => {
     const API_URL = "https://bio.torre.co/api/bios";
+
     const { username } = req.query;
 
     try {
-        const response = await fetch(`${API_URL}/${username}`);
-        const data = await response.json();
-        return res.send(data);
+        const response =  await axios.get(`${API_URL}/${username}`);
+        
+        res.send(response.data.person.location);
+        
     } catch (error) {
-        return res.send(error);
-    }
-    
+        if (error.response) {
+            res
+            .status(error.response.status)
+            .send(error.response.data);
+          } else if (error.request) {
+            res.status()
+            .send(`Can't finish request properly, please try again`);
+          } else {
+            res.status(500).send('We fucked up');
+          }
+    }        
 }
--- src/services/TorreService.js
+++ src/services/TorreService.js
@@ -1,10 +1,10 @@
 import axios from "axios";
-const API_URL = "https://bio.torre.co/api/bios";
+const API_URL = "/api/hello";
 
 export default {
     async getUser(username) {
         try {
-            return await axios.get(`${API_URL}/${username}`);
+            return await axios.get(`${API_URL}?username=${username}`);
         } catch (error) {
             console.error(error);
             return error;
